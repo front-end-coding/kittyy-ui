d0e0ae7fca87931602c772bb564cac0b60581687
guoxiaoyang395 <guo8976540500@163.com>
2022-08-11 19:50:09 +0800

    feat(): update types file

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue"
-import dts from 'vite-plugin-dts'
+// import dts from 'vite-plugin-dts'
 import { resolve } from 'path'
 export default defineConfig({
   build: {
@@ -45,16 +45,16 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    dts({
-      outputDir: './dist/es',
-      //指定使用的tsconfig.json为我们整个项目根目录,如果不配置,你也可以在components下新建tsconfig.json
-      tsConfigFilePath: '../../tsconfig.json'
-    }),
-    //因为这个插件默认打包到es下，我们想让lib目录下也生成声明文件需要再配置一个
-    dts({
-      outputDir: './dist/lib',
-      tsConfigFilePath: '../../tsconfig.json'
-    }),
+    // dts({
+    //   outputDir: './dist/es',
+    //   //指定使用的tsconfig.json为我们整个项目根目录,如果不配置,你也可以在components下新建tsconfig.json
+    //   tsConfigFilePath: '../../tsconfig.json'
+    // }),
+    // //因为这个插件默认打包到es下，我们想让lib目录下也生成声明文件需要再配置一个
+    // dts({
+    //   outputDir: './dist/lib',
+    //   tsConfigFilePath: '../../tsconfig.json'
+    // }),
     // 针对buil的roolup的钩子
     {
       name: 'style',
